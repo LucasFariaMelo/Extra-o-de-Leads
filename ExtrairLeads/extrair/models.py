0cc287b6a56086c0bf151d9a4297e64ec4a7edd0
lucas <lucasfariamelo307@gmail.com>
2023-12-21 07:46:13 -0300

    Criando o template dos leads

--- ExtrairLeads/extrair/models.py
+++ ExtrairLeads/extrair/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Lead(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    telefone = models.CharField(max_length=20, blank=True, null=True)
+    status = models.CharField(max_length=20, default='Topo de Funil')
+
+    def __str__(self):
+        return self.nome
--- ExtrairLeads/extrair/views.py
+++ ExtrairLeads/extrair/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Lead
 
 # Create your views here.
+
+def lista_leads(request):
+    leads = Lead.objects.all()
+    return render(request, 'lista_leads.html', {'leads': leads})
