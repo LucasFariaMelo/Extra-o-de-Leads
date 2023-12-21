12424852914ea4f9ae084e4145039344518558e4
lucas <lucasfariamelo307@gmail.com>
2023-12-21 08:06:40 -0300

    Incluindo com sucesso a extração de leads

--- ExtrairLeads/extrair/urls.py
+++ ExtrairLeads/extrair/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.lista_leads, name='lista_leads'),
+    path('', views.index, name='pagina_inicial'),
+    path('criar_lead/', views.criar_lead, name='criar_lead'),
+    path('leads/', views.lista_leads, name='lista_leads'),
 ]
--- ExtrairLeads/extrair/views.py
+++ ExtrairLeads/extrair/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Lead
+from .forms import LeadForm
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
 
 def lista_leads(request):
     leads = Lead.objects.all()
     return render(request, 'lista_leads.html', {'leads': leads})
+
+def criar_lead(request):
+    if request.method == 'POST':
+        form = LeadForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_leads')
+    else:
+        form = LeadForm()
+    return render(request, 'index.html', {'form': form})
